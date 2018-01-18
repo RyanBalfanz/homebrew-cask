375d5cc2d203ad191448b0a9e0899eddb762b994
Ryan Balfanz <rbalfanz@gmail.com>
2018-01-17 17:57:18 -0800

    Upgrade Supervisor to v0.1.0-alpha.2

--- Casks/supervisor.rb
+++ Casks/supervisor.rb
@@ -1,6 +1,6 @@
 cask 'supervisor' do
-  version '0.1.0-alpha'
-  sha256 '673d405d52ebe5274a4e4d9b11f772a3b816af2025fb2dc70f67b92724126ecf'
+  version '0.1.0-alpha.2'
+  sha256 'aad2784802bc3a40d267f7671ba50f75477f604964315adeff961813d3b01a44'
 
   url "https://github.com/RyanBalfanz/SupervisorApp/releases/download/v#{version}/Supervisor-darwin-x64.zip"
   name 'Supervisor'
